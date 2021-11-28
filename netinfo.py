44466e64075db0daa6e1080d669834db46239155
David Cruz <david@thecruzs.net>
2021-11-28 14:35:43 -0800

    add analyze_path and refactor

--- netinfo.py
+++ netinfo.py
@@ -5,7 +5,9 @@ import json
 import os
 import time
 from netbrain import NetBrain
+from netmiko import ConnectHandler
 
+cwd = os.getcwd()
 router_types = ["Cisco", "Palo Alto Networks"]
 
 
@@ -42,6 +44,8 @@ def get_active_gateway(nb, src_ip):
             if nb.get_pan_ha_state(device_attrs["name"]) == "passive":
                 continue
 
+        device_attrs.update({"srcIP": gw["ip"]})
+
         return device_attrs
 
 
@@ -64,13 +68,59 @@ def parse_args():
     return parser.parse_args()
 
 
+def analyze_path(mgmt_ip, src_ip, dst_ip, credentials, vendor):
+    """Analyze the path between the source and destination"""
+
+    vendors = {
+        "Cisco": {
+            "ssh": {
+                "device_type": "cisco_ios",
+                "host": mgmt_ip,
+                "username": credentials["Cisco"]["username"],
+                "password": credentials["Cisco"]["password"],
+            },
+            "ttp_template": fr"{cwd}\cisco.ttp",
+            "commands": [
+                f"ping {dst_ip} source {src_ip}",
+                f"traceroute {dst_ip} source {src_ip}",
+            ],
+        },
+        "Palo Alto Networks": {
+            "ssh": {
+                "device_type": "paloalto_panos",
+                "host": mgmt_ip,
+                "username": credentials["Palo Alto Networks"]["username"],
+                "password": credentials["Palo Alto Networks"]["password"],
+            },
+            "ttp_template": fr"{cwd}\pan.ttp",
+            "commands": [
+                f"ping count 4 source {src_ip} host {dst_ip}",
+                f"traceroute source {src_ip} host {dst_ip}",
+            ],
+        },
+    }
+
+    output = list()
+    with ConnectHandler(**vendors[vendor]["ssh"]) as net_connect:
+        for cmd in vendors[vendor]["commands"]:
+            output.append(
+                net_connect.send_command_timing(
+                    cmd,
+                    strip_prompt=True,
+                    strip_command=True,
+                    use_ttp=False,
+                    ttp_template=vendors[vendor]["ttp_template"],
+                )
+            )
+
+    return output
+
+
 def main():
-    t1_start = time.process_time()
+    t1_start = time.time()
 
     args = parse_args()
-
     env = import_env("env.json")
-
     nb = NetBrain(
         env["netbrain_url"],
         env["netbrain_user"],
@@ -78,12 +128,19 @@ def main():
         env["tenant_name"],
         env["domain_name"],
     )
-
     device_attrs = get_active_gateway(nb, args.source)
+    # print(json.dumps(device_attrs, indent=2, sort_keys=True))
 
-    print(json.dumps(device_attrs, indent=2, sort_keys=True))
+    results = analyze_path(
+        device_attrs["mgmtIP"],
+        device_attrs["srcIP"],
+        args.destination,
+        env["credentials"],
+        device_attrs["vendor"],
+    )
+    print("\n".join(results))
 
-    t1_stop = time.process_time()
+    t1_stop = time.time()
     print(f"\n Took {t1_stop-t1_start :.3f} seconds to complete")
 
 
